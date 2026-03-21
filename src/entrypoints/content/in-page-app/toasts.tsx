7badebc750c89e20a6277fe5399957eeebd5df2c
bn-dev-042 <255405198+bn-dev-042@users.noreply.github.com>
2026-03-21 13:45:57 +0000

    Improve data warmup toast show/hide logic (#99)

--- src/entrypoints/content/in-page-app/toasts.tsx
+++ src/entrypoints/content/in-page-app/toasts.tsx
@@ -105,7 +105,7 @@ export function Toasts() {
     return (
       <React.Suspense>
         <ToastWithDataWarmup
-          onClose={() => {
+          onDone={() => {
             setDataWarmupToastNeeded(false);
           }}
         />
