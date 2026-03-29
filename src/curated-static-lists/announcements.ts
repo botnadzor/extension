5b21606250b9abadbb95d8fc126d5e2a4ad21ea0
bn-dev-042 <255405198+bn-dev-042@users.noreply.github.com>
2026-03-29 20:20:08 +0100

    Improve curated static list configs (#147)

--- src/curated-static-lists/announcements.ts
+++ src/curated-static-lists/announcements.ts
@@ -3,11 +3,6 @@ import type { z } from "zod/mini";
 import type { interpretedAnnouncementListItemSchema } from "@/shared/@model/static-lists/=announcements";
 
 export default [
-  {
-    createdAt: "2026-01-11T21:00:00Z",
-    extensionVersionRange: "*",
-    header: "Расширение обновлено до альфа-версии 2.0",
-    content:
-      "Рады представить вам новую версию расширения! В ней мы переработали внутреннее устройство функционирования расширения и подготовили его для работы с постоянно растущим количеством аккаунтов для подсветки",
-  },
+  // This file is a playground for testing list items.
+  // Do not merge changes to this file into the main branch.
 ] satisfies Array<z.input<typeof interpretedAnnouncementListItemSchema>>;
